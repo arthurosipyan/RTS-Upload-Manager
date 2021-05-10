3ef4d40493d90ccd99338e7f18ba576a76c792ce
Arthur Osipyan <arthurosipyan97@gmail.com>
2021-05-10 13:16:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,31 +3,39 @@
 # Example of spreadsheet column format:
 # ClientNo (0) | Invoice# (1) | DebtorNo (2) | DebtorName (3) | Pono (4) | InvDate (5) | InvAmt (6)
 
-# intended output: "OT1900" "OT1901" "OT1903"
+# example output: "OT1900" "OT1901" "OT1903"
 from tkinter import Tk     # from tkinter import Tk for Python 3.x
 from tkinter.filedialog import askopenfilename
 import pandas as pd
 
-# Method to get total count of invoices
 
-# Method to get total amount
+def total_invoices(file):
+    return len(rts_upload(file, 'Invoice#'))
 
 
-def rts_upload():
-    Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
-    filename = askopenfilename()  # show an "Open" dialog box and return the path to the selected file
+def total_revenue(file):
+    revenue = 0
+    for x in rts_upload(file, 'InvAmt'):
+        revenue += x
+    return revenue
 
-    print(filename)
-    df = pd.read_excel(filename)
 
-    return df['Invoice#'].tolist()
+def invoices(file):
+    upload_files = ""
+    for file in rts_upload(file, 'Invoice#'):
+        upload_files += '\"' + file + '\" '
+    return upload_files
+
+
+def rts_upload(file, column):
+    df = pd.read_excel(file)
+    return df[column].tolist()
 
 
 if __name__ == '__main__':
-    upload_files = ""
-    count = 0
-    for file in rts_upload():
-        upload_files += '\"' + file + '\" '
-        count += 1
-    print("Total Invoices: ", count)
-    print(upload_files)
+    Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
+    filename = askopenfilename()  # show an "Open" dialog box and return the path to the selected file
+    print("File:", filename)
+    print("Total Invoices:", total_invoices(filename))
+    print("Total Revenue:", total_revenue(filename))
+    print("Invoices:", invoices(filename))
