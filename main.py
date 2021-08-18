099d50bcfd30fd130c7c69ff90a8849993d50a53
Arthur Osipyan <33210928+arthurosipyan@users.noreply.github.com>
2021-08-17 23:14:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,34 @@
 # ClientNo (0) | Invoice# (1) | DebtorNo (2) | DebtorName (3) | Pono (4) | InvDate (5) | InvAmt (6)
 
-from tkinter import Tk     # from tkinter import Tk for Python 3.x
+import os
+from tkinter import Tk  # from tkinter import Tk for Python 3.x
 from tkinter.filedialog import askopenfilename
 import pandas as pd
 
 
-def df_column(file, column):
-    df = pd.read_excel(file)
-    return df[column]
+def get_revenue(sheet):
+    return "${:,.2f}".format(sheet.sum())
 
 
-def invoices(file):
-    upload_files = ""
-    count = 0
-    for file in df_column(file, 'Invoice#').tolist():
-        if count <= 30:
-            upload_files += '\"' + file + '\" '
-            count += 1
-        else:
-            upload_files += '\n'
-            count = 0
-    return upload_files
+def get_row_count(sheet):
+    return len(sheet.index)
+
+
+def print_invoices(sheet):
+    print("Invoices:\n")
+    for invoice in sheet.tolist():
+        print("\"" + invoice + "\"")
 
 
 if __name__ == '__main__':
     Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
     filename = askopenfilename()  # show an "Open" dialog box and return the path to the selected file
-    print("File:", filename)
-    print("Total Invoices:", len(df_column(filename, 'Invoice#').tolist()))
-    print("Total Revenue:", "${:,.2f}".format(df_column(filename, 'InvAmt').sum()))
-    print(invoices(filename))
+    df = pd.read_excel(filename, sheet_name=-1)  # create df of spreadsheet
+
+    print("File:", os.path.basename(filename))
+    print("Total Invoices:", get_row_count(df))
+    print("Total Revenue:", get_revenue(df.iloc[:, -1]))
+    print_invoices(df.iloc[:, 1])
 
 # TODO:
 # - Tkinter GUI
